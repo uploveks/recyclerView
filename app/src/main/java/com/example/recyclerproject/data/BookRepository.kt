ed2aacef4bffab6d17d13781a8c5c862c9387a02
uploveks <daniela.belibov@stud.acs.upb.ro>
2023-07-26 16:53:15 +0300

    fixed bugs for retrofit (certificates) in mvvm

--- app/src/main/java/com/example/recyclerproject/data/BookRepository.kt
+++ app/src/main/java/com/example/recyclerproject/data/BookRepository.kt
@@ -1,8 +1,15 @@
 import com.example.recyclerproject.model.Book
 import com.example.recyclerproject.network.ApiInterface
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class BookRepository {
     companion object {
@@ -12,14 +19,41 @@ class BookRepository {
     private val apiInterface: ApiInterface
 
     init {
+        val trustAllCertificates = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                @Throws(CertificateException::class)
+                override fun checkClientTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?
+                ) {
+                }
+
+                @Throws(CertificateException::class)
+                override fun checkServerTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?
+                ) {
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            }
+        )
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustAllCertificates, SecureRandom())
+
+        val unsafeOkHttpClient = OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCertificates[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(JSON_KEEPER)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(unsafeOkHttpClient)
             .build()
 
         apiInterface = retrofit.create(ApiInterface::class.java)
     }
-    //dsucgdegckud
 
     suspend fun getBooks(): Response<List<Book>> {
         return apiInterface.getBooks()
